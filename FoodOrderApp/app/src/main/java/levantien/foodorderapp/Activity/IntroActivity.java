d25de9c06a16323ef9b3bd497ef18d8ba1c8eeb7
Tiến <letien3413@gmail.com>
2024-05-14 19:33:38 +0700

    tạo MainActivity và thiết kế layout activity_intro

--- FoodOrderApp/app/src/main/java/levantien/foodorderapp/Activity/IntroActivity.java
+++ FoodOrderApp/app/src/main/java/levantien/foodorderapp/Activity/IntroActivity.java
@@ -2,14 +2,29 @@ package levantien.foodorderapp.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import levantien.foodorderapp.R;
+import levantien.foodorderapp.databinding.ActivityIntroBinding;
 
-public class IntroActivity extends AppCompatActivity {
+public class IntroActivity extends BaseActivity {
+    ActivityIntroBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
+        binding = ActivityIntroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setVariable();
+    }
+
+    private void setVariable() {
+        binding.btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroActivity.this, MainActivity.class));
+            }
+        });
     }
 }
