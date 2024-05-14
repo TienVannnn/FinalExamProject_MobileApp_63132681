486a693dbcd2e0dd5b8bd4b060052dca58518d19
Tiến <letien3413@gmail.com>
2024-05-14 19:15:17 +0700

    sửa MainActivity thành IntroActivity

--- FoodOrderApp/app/src/main/java/levantien/foodorderapp/Activity/IntroActivity.java
+++ FoodOrderApp/app/src/main/java/levantien/foodorderapp/Activity/IntroActivity.java
@@ -1,14 +1,16 @@
-package levantien.foodorderapp;
+package levantien.foodorderapp.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import levantien.foodorderapp.R;
+
+public class IntroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_intro);
     }
 }
