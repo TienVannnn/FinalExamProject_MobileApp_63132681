734ff01bb685fed616ed59ee116bba074dd55777
Tiến <letien3413@gmail.com>
2024-05-16 09:47:29 +0700

    thiết kế layout activity_listfood

---
+++ FoodOrderApp/app/src/main/java/levantien/foodorderapp/Activity/ListFoodActivity.java
@@ -0,0 +1,16 @@
+package levantien.foodorderapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import levantien.foodorderapp.R;
+
+public class ListFoodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_food);
+    }
+}
