b1026570bc21b588d31d1742aff5fadae374c95f
Tiến <letien3413@gmail.com>
2024-05-17 19:27:16 +0700

    thiết kế layout login, signup, profile

---
+++ FoodOrderApp/app/src/main/java/levantien/foodorderapp/Activity/SignupActivity.java
@@ -0,0 +1,16 @@
+package levantien.foodorderapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import levantien.foodorderapp.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
