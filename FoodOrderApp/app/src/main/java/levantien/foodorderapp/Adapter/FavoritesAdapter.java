3ff6f6f6065ed86b6300e3a7c1a4b4befe0dac87
Tiến <letien3413@gmail.com>
2024-05-24 12:08:56 +0700

    tạo adapter favorites và xử lý load sản phẩm yêu thích lên giao diện

---
+++ FoodOrderApp/app/src/main/java/levantien/foodorderapp/Adapter/FavoritesAdapter.java
@@ -0,0 +1,66 @@
+package levantien.foodorderapp.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+
+import java.util.ArrayList;
+
+import levantien.foodorderapp.Domain.Foods;
+import levantien.foodorderapp.R;
+
+public class FavoritesAdapter extends RecyclerView.Adapter<FavoritesAdapter.FavaritesViewHolder> {
+    ArrayList<Foods> ds;
+    Context context;
+
+    public FavoritesAdapter(ArrayList<Foods> ds, Context context) {
+        this.ds = ds;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public FavaritesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.viewholder_fav, parent, false);
+        return new FavaritesViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FavaritesViewHolder holder, int position) {
+        Foods selected = ds.get(position);
+        holder.title.setText(selected.getTitle());
+        holder.time.setText(selected.getTimeValue() + "min");
+        holder.price.setText("$" + selected.getPrice());
+        holder.rating.setText("" + selected.getStar());
+        Glide.with(holder.itemView.getContext()).load(selected.getImagePath()).transform(new CenterCrop(), new RoundedCorners(30)).into(holder.pic);
+    }
+
+    @Override
+    public int getItemCount() {
+        return ds.size();
+    }
+
+
+    public static class FavaritesViewHolder extends RecyclerView.ViewHolder{
+        TextView title, price, rating, time;
+        ImageView pic;
+        public FavaritesViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.tvTitleF);
+            time = itemView.findViewById(R.id.tvTimeF);
+            price = itemView.findViewById(R.id.tvPriceF);
+            rating = itemView.findViewById(R.id.tvRatingF);
+            pic = itemView.findViewById(R.id.imgF);
+        }
+    }
+}
--- FoodOrderApp/app/src/main/java/levantien/foodorderapp/Fragment/FavoritesFragment.java
+++ FoodOrderApp/app/src/main/java/levantien/foodorderapp/Fragment/FavoritesFragment.java
@@ -1,22 +1,90 @@
 package levantien.foodorderapp.Fragment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import levantien.foodorderapp.Adapter.FavoritesAdapter;
+import levantien.foodorderapp.Domain.Foods;
 import levantien.foodorderapp.R;
+import levantien.foodorderapp.databinding.FragmentFavoritesBinding;
+import levantien.foodorderapp.databinding.FragmentHomeBinding;
 
 
 public class FavoritesFragment extends Fragment {
+    private FragmentFavoritesBinding binding;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference usersReference;
+    String phoneId;
+    boolean isLogin;
+    private ArrayList<Foods> favoriteFoods = new ArrayList<>();
+    private FavoritesAdapter favoritesAdapter;
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorites, container, false);
+        binding = FragmentFavoritesBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SharedPreferences sharedPreferences = getContext().getSharedPreferences("UserPrefs", Context.MODE_PRIVATE);
+        isLogin = sharedPreferences.getBoolean("isLogin", false);
+        usersReference = database.getReference("users");
+        phoneId = sharedPreferences.getString("phoneId", "");
+
+        setupRecyclerView();
+        if (isLogin) {
+            loadFavoriteFoods();
+        }
+
+    }
+
+    private void setupRecyclerView() {
+        favoritesAdapter = new FavoritesAdapter(favoriteFoods, getContext());
+        binding.recycleFav.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.recycleFav.setAdapter(favoritesAdapter);
+    }
+
+    private void loadFavoriteFoods() {
+        usersReference.child(phoneId).child("favoritesFood").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                favoriteFoods.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Foods food = dataSnapshot.getValue(Foods.class);
+                    favoriteFoods.add(food);
+                }
+                favoritesAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Handle possible errors.
+            }
+        });
     }
 }
